cab8e3d78a78c8c775600654c737e1f43482be84
Nico Contereras <nico@data-gourmet.com>
2024-02-14 14:21:47 +0000

    checked query

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,10 +1,10 @@
 SELECT 
-    products_id, 
-    date_date, 
-    orders_id,
-    revenue, 
-    quantity, 
-    CAST(purchase_price AS FLOAT64), 
+    s.products_id, 
+    s.date_date, 
+    s.orders_id,
+    s.revenue, 
+    s.quantity, 
+    CAST(p.purchase_price AS FLOAT64), 
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
     s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 FROM {{ref("stg_raw__sales")}} s
