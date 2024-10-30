87e7f7e41a44eb294301d99304e074d88832059d
Yann Rabasse <yann94380@gmail.com>
2024-10-30 17:25:59 +0000

    Challenge 4 - 2 (macro in model)

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,3 +1,4 @@
+WITH tab1 as (
 SELECT 
     s.products_id, 
     s.date_date, 
@@ -6,7 +7,12 @@ SELECT
     s.quantity, 
     CAST(p.purchase_price AS FLOAT64) as purchase_price_ok, 
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    ROUND (s.revenue - (s.quantity*CAST(p.purchase_price AS FLOAT64)),2) AS margin,
 FROM {{ref("stg_raw__sales")}} s
 LEFT JOIN {{ref("stg_raw__product")}} p 
     USING (products_id)
+)
+SELECT 
+    tab1.*,
+    {{ margin_percent('revenue', 'purchase_cost') }} AS margin_percent
+FROM tab1
