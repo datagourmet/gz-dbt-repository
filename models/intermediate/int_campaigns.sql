9baf432e23948beda4f4aca4f8622b680801b0df
Yann Rabasse <yann94380@gmail.com>
2024-10-30 17:34:19 +0000

    Challenge4 -3 macros from package

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,14 +1,5 @@
-with tab1 as (SELECT *
-FROM {{ ref("stg_raw__adwords") }}
-UNION ALL
-SELECT *
-FROM {{ ref("stg_raw__bing") }}
-UNION ALL
-SELECT *
-FROM {{ ref("stg_raw__criteo") }}
-UNION ALL
-SELECT *
-FROM {{ ref("stg_raw__facebook") }})
-select *
-from tab1
-order by date_date
+{{
+     dbt_utils.union_relations(
+         relations=[ref('stg_raw__adwords'), ref('stg_raw__bing'), ref('stg_raw__criteo'), ref('stg_raw__facebook')]
+     )
+ }}
